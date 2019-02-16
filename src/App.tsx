f47d750ba8d6027a2ee941b61e93b402100d706b
Yash Mathur <yash.mathur@grandrounds.com>
2019-02-15 18:36:51 -0800

    create routes; create context to maintain app state

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import FormContext from "./contexts/FormContext";
+import Dashboard from "./sections/Dashboard";
+import QuestionSet from "./sections/QuestionSet";
 
 class App extends Component {
+  FETCH_URL: string = "https://hackthenorth.com/fe-questions.json";
+
+  constructor(props) {
+    super(props);
+
+    window.onbeforeunload = event => {
+      event.preventDefault();
+      event.returnValue = "";
+    };
+  }
+
+  saveFormInContext(formData: any) {
+    const newData = formData;
+
+    formData.forEach((questionSet: any, i: number) => {
+      questionSet.questions.forEach((_question: any, j: number) => {
+        formData[i].questions[j].answer = "";
+      });
+    });
+
+    this.setState({ newData, updateAnswer: this.updateAnswer });
+  }
+
+  componentDidMount() {
+    fetch(this.FETCH_URL)
+      .then(res => res.json())
+      .then(res => {
+        this.saveFormInContext(res);
+      });
+  }
+
+  updateAnswer = (set, questionId, answer) => {
+    this.state["newData"][set].questions[questionId].answer = answer;
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <FormContext.Provider value={this.state}>
+          <Router>
+            <div>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/sets/:id" component={QuestionSet} />
+            </div>
+          </Router>
+        </FormContext.Provider>
       </div>
     );
   }
